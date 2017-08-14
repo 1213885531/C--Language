54a77c470b01f35f3e0776c11d48748489717d3a
1213885531 <1213885531@qq.com>
2017-08-14 09:51:48 +0800

    Signed-off-by: 1213885531 <1213885531@qq.com>

---
+++ gets.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char str[1];
+    scanf("%s",str);
+    printf("%s",str);
+
+
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Hello world!\n");
+    scanf("%3d",&i);//Hello world!
+                    //36521
+                    //365
+                    //最大是999，最小是-99
+    printf("%d\n",i);
+    return 0;
+}
